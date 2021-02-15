78af487e27e6f0cb63a6ae8c2ff8cae36bfbd5cd
Andrew Ahler <andrewahler@gmail.com>
2021-02-15 15:50:21 -0600

    added homepage

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -9,7 +9,6 @@ router.get('/', (req, res) => {
   Post.findAll({
     attributes: [
       'id',
-      'post_url',
       'title',
       'created_at',
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
